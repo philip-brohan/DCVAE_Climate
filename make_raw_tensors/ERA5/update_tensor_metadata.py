f8fd013fdefde21587a5bcae88e335dc89bfcb46
Philip Brohan <philip@brohan.org>
2024-07-09 16:22:00 +0100

    Add diagnostics to warn about missing raw tensors.

--- make_raw_tensors/ERA5/update_tensor_metadata.py
+++ make_raw_tensors/ERA5/update_tensor_metadata.py
@@ -30,11 +30,34 @@ fn = "%s/DCVAE-Climate/raw_datasets/ERA5/%s_zarr" % (
 zarr_ds = zarr.open(fn, mode="r+")
 
 AvailableMonths = {}
+start = "%04d-%02d" % (LastYear, 12)
+end = "%04d-%02d" % (FirstYear, 1)
 for year in range(FirstYear, LastYear + 1):
     for month in range(1, 13):
+        dte = "%d-%02d" % (year, month)
         idx = date_to_index(year, month)
         slice = zarr_ds[:, :, idx]
         if not np.all(np.isnan(slice)):
             AvailableMonths["%d-%02d" % (year, month)] = idx
+            if dte < start:
+                start = dte
+            if dte > end:
+                end = dte
 
 zarr_ds.attrs["AvailableMonths"] = AvailableMonths
+
+missing = 0
+for year in range(FirstYear, LastYear + 1):
+    for month in range(1, 13):
+        dte = "%d-%02d" % (year, month)
+        if dte < start or dte > end:
+            continue
+        if dte not in AvailableMonths:
+            missing += 1
+
+print(args.variable)
+print("Start date:", start)
+print("End date:", end)
+print("Missing months:", missing)
+print("Total months:", len(AvailableMonths))
+print("\n")
